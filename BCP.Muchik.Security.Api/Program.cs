6bcff0bb5af254bff68fbebac2fe081655833bb0
Jordan Cruz <jordancruz@bcp.com.pe>
2023-10-15 00:35:47 -0500

    feat(security): Add jwt support

--- BCP.Muchik.Security.Api/Program.cs
+++ BCP.Muchik.Security.Api/Program.cs
@@ -1,3 +1,13 @@
+using BCP.Muchik.Infrastructure.CrossCutting.Jwt;
+using BCP.Muchik.Security.Api.Middleware;
+using BCP.Muchik.Security.Application.Interfaces;
+using BCP.Muchik.Security.Application.Mappings;
+using BCP.Muchik.Security.Application.Services;
+using BCP.Muchik.Security.Domain.Interfaces;
+using BCP.Muchik.Security.Infrastructure.Context;
+using BCP.Muchik.Security.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//SQL Server
+builder.Services.AddDbContext<SecurityContext>(config =>
+{
+    config.UseSqlServer(builder.Configuration.GetConnectionString("SecurityConnection"));
+    //config.UseSqlServer(builder.Configuration.GetValue<string>("connectionStrings:SecurityConnection"));
+});
+//Automapper
+builder.Services.AddAutoMapper(typeof(DtoToEntityProfile));
+//Services
+builder.Services.AddTransient<ISecurityService, SecurityService>();
+//Repositories
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+//Context
+builder.Services.AddTransient<SecurityContext>();
+//CrossCutting
+builder.Services.AddTransient<IJwtManager, JwtManager>();
 
 var app = builder.Build();
 
@@ -16,6 +43,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<GlobalExceptionMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ BCP.Muchik.Security.Application/Dtos/CreateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BCP.Muchik.Security.Application.Dtos
+{
+    public class CreateUserDto
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
